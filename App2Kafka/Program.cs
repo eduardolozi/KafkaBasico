0529331a3c33c4ef5ec072d020d855abfe5deaf3
Eduardo Lozano <eduardoellozi2023@gmail.com>
2024-07-23 13:46:23 -0300

    add readme

--- App2Kafka/Program.cs
+++ App2Kafka/Program.cs
@@ -1,7 +1,7 @@
 ﻿using App2Kafka;
 var producer = new Producer();
 await producer.CriarServidor();
-await producer.BalanceiaTopicoSemConsumer();
+//await producer.BalanceiaTopicoSemConsumer();
 await producer.EnviarMensagem("Esse é o Topico 2");
 await producer.EnviarMensagem("Teste da segunda aplicacao");
 await producer.EnviarMensagem("Kafka é muito massa");
---
+++ readme.md
@@ -0,0 +1,5 @@
+# GUIA PARA A APLICACAO
+
+
+## Comando para abrir o Bash do Kafka no Docker
+docker exec -it kafkamultiplasversoes-kafka-1 /bin/bash
